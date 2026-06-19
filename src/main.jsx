0edbb4bc71e7cd08d772bc5056624a4d15b21f18
Evelina <evelinaavegas@gmail.com>
2026-06-19 18:31:00 +0300

    Footer container create

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header  from './Header/Header.jsx'
 import Content from './Content/Content.jsx'
+import Footer from './Footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Content />
+    <Footer />
   </StrictMode>,
 )
